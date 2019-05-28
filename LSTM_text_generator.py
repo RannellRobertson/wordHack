7713937a2a0802c9834a446e7f43f1a90978490f
RannellRobertson <32848166+RannellRobertson@users.noreply.github.com>
2019-05-27 20:29:37 -0400

    Update LSTM_text_generator.py

--- LSTM_text_generator.py
+++ LSTM_text_generator.py
@@ -3,14 +3,11 @@ from fetch_text import open_file
 import tensorflow as tf
 import keras
 from preprocessing import Preprocessor
-from LSTM_models import ultimate_lstm_model
+from LSTM_model import lstm_model
 from pprint import pprint
 
 
-config = tf.ConfigProto(device_count={"CPU": 8})
-keras.backend.tensorflow_backend.set_session(tf.Session(config=config))
-
-
+# Preprocess the text that the LSTM model can use.
 text = open_file("edgar_allan_poe/edgar_allan_poe_complete_works.txt")
 preprocessor = Preprocessor(text)
 x_modified, y_modified = preprocessor.preprocessor()
@@ -18,11 +15,14 @@ x_modified, y_modified = preprocessor.preprocessor()
 string_mapped = preprocessor.X[99]
 full_string = [preprocessor.n_to_char[value] for value in string_mapped]
 
+
+# train the model for 100 epochs then load the 
+# the saved weights and units of the model.
 model = ultimate_lstm_model(x_modified, y_modified)
 
-model.load_weights("text_generator_700_0.2_700_0.2_700_0.2_ultimate.h5")
+model.load_weights("text_generator_700_0.2_700_0.2_700_0.2.h5")
 
-# generating character
+# generate characters
 
 for i in range(400):
     x = np.reshape(string_mapped, (1, len(string_mapped), 1))
@@ -42,7 +42,7 @@ for char in full_string:
     txt += char
 
 # write generated text to a file
-
+# print the text to console.
 with open("poem_text", "w") as f:
     f.write(txt)
     pprint(txt)
